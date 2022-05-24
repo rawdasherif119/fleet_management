351e88fbc32d32965ec308a9af94305f08fc9441
rawdasherif <rawda.sherif@ibtikar.net.sa>
2022-05-24 03:08:09 +0200

    Project Base structure

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,8 +1,16 @@
 <?php
-
 namespace App\Exceptions;
 
+use App\Resources\ErrorResource;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -10,7 +18,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<Throwable>>
+     * @var array
      */
     protected $dontReport = [
         //
@@ -19,7 +27,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $dontFlash = [
         'current_password',
@@ -32,10 +40,63 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
+    public function report(Throwable $exception)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        if ($exception instanceof \League\OAuth2\Server\Exception\OAuthServerException) {
+            return;
+        }
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            abort(new ErrorResource(Response::HTTP_METHOD_NOT_ALLOWED, __('errors.method_not_allowed')));
+        }
+        if ($exception instanceof NotFoundHttpException) {
+            abort(new ErrorResource(Response::HTTP_NOT_FOUND, __('errors.route_not_found')));
+        }
+        if ($exception instanceof ModelNotFoundException) {
+            abort(new ErrorResource(Response::HTTP_NOT_FOUND, __('errors.model_not_found')));
+        }
+        if ($exception instanceof UnauthorizedException
+            || $exception instanceof \Spatie\Permission\Exceptions\UnauthorizedException
+            || $exception instanceof AuthorizationException
+        ) {
+            abort(new ErrorResource(Response::HTTP_FORBIDDEN, __('errors.unauthorized')));
+        }
+        return parent::render($request, $exception);
+    }
+
+    /**
+     * Convert a validation exception into a JSON response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Validation\ValidationException  $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        $errors = [];
+        foreach ($exception->errors() as $field => $message) {
+            $errors[] = [
+                'type'  => $field,
+                'error' => $message[0],
+            ];
+        }
+        return response()->json(['errors' => $errors], $exception->status);
+    }
+
+    /**
+     * Hndel unauthenticated exception.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson()
+        ? response()->json(['message' => $exception->getMessage()], 401)
+        : redirect()->guest(route('login'));
     }
 }
---
+++ app/Services/BaseService.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\BaseRepositoryInterface;
+use App\Traits\ManageFiles;
+
+class BaseService
+{
+    use ManageFiles;
+
+    protected $repo;
+
+    public function __construct(BaseRepositoryInterface $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    /**
+     * @param array $relations
+     * @param array $scopes
+     * @param array $columns
+     * @param string $orders
+     */
+    public function getData(
+        $relations = [], $scopes = [], $orders = ['created_at' => 'DESC'], $columns = ['*']
+    ) {
+        $this->repo->setRelations($relations);
+        $this->repo->setScopes($scopes);
+        return $this->repo->getData($columns, $orders);
+    }
+
+    /**
+     * @param array $relations
+     * @param array $scopes
+     * @param array $columns
+     * @param string $orders
+     */
+    public function getPaginated(
+        $parPage = 15, $relations = [], $scopes = [], $orders = ['created_at' => 'DESC'], $columns = ['*']
+    ) {
+        $this->repo->setRelations($relations);
+        $this->repo->setScopes($scopes);
+        return $this->repo->getPaginated($parPage, $columns, $orders);
+    }
+
+    /**
+     * @param  integer $id
+     * @param array $relations
+     * @param array $columns
+     */
+    public function show($id, $relations = [], $columns = ['*'])
+    {
+        $this->repo->setRelations($relations);
+        return $this->repo->show($id, $columns);
+    }
+
+    /**
+     * @param array $data
+     * @param $relation
+     */
+    public function create($data, $relation = null)
+    {
+        return $this->repo->create($data, $relation);
+    }
+
+    /**
+     * @param array $data
+     * @param $relation
+     */
+    public function createWithRelations($data, $relation = null)
+    {
+        return $this->repo->createWithRelations($data, $relation);
+    }
+
+    /**
+     * @param array $data
+     * @param int|object $model
+     */
+    public function updateWithRelation($data, $model)
+    {
+        return $this->repo->updateWithRelation($data, $model);
+    }
+
+    /**
+     * @param array $data
+     * @param int|object $model
+     */
+    public function update($data, $model)
+    {
+        return $this->repo->update($data, $model);
+    }
+
+    /**
+     * @param  $attr
+     * @param  $value
+     */
+    public function updateOrCreate($attr, $value, $relation = null)
+    {
+        return $this->repo->updateOrCreate($attr, $value, $relation);
+    }
+
+    /**
+     * @param object $model
+     * @param array $data
+     */
+    public function createRelations($model, $data)
+    {
+        return $this->repo->createRelations($model, $data);
+    }
+
+    /**
+     * @param object $model
+     * @param array $data
+     */
+    public function updateRelations($model, $data)
+    {
+        return $this->repo->updateRelations($model, $data);
+    }
+
+    /**
+     * @param int|array $id
+     */
+    public function delete($id)
+    {
+        return $this->repo->delete($id);
+    }
+
+    /**
+     * @param string $relation
+     * @param array $ids
+     * @param array $pivots
+     */
+    public function sync($relation, $ids, $pivots = [])
+    {
+        return $this->repo->sync($relation, $ids, $pivots);
+    }
+
+    /**
+     * @param string $relation
+     * @param int $perPage
+     * @param string $orderBy
+     * @param string $sort
+     */
+    public function paginateRelation($relation, $perPage = 15, $columns = ['*'],
+        $orderBy = 'created_at', $sort = 'DESC'
+    ) {
+        return $this->repo->paginateRelation($relation, $perPage, $columns, $orderBy, $sort);
+    }
+
+    /**
+     * @param $id
+     * @param array $columns
+     */
+    public function findOrFail($id, $columns = ['*'])
+    {
+        return $this->repo->findOrFail($id, $columns);
+    }
+    /**
+     * @param $attribute
+     * @param $value
+     * @param array $relations
+     * @param array $columns
+     */
+    public function findBy($attribute, $value, $relations = [], $columns = ['*'])
+    {
+        return $this->repo->findBy($attribute, $value, $relations, $columns);
+    }
+
+    /**
+     * @param string attribute
+     * @param array $values
+     * @param array $relations
+     */
+    public function whereIn($attribute, array $values, $relations = [])
+    {
+        $this->repo->setRelations($relations);
+        return $this->repo->whereIn($attribute, $values);
+    }
+
+    /**
+     * @param string attribute
+     * @param array $values
+     * @param array $relations
+     */
+    public function whereNotIn($attribute, array $values, $relations = [])
+    {
+        $this->repo->setRelations($relations);
+        return $this->repo->whereNotIn($attribute, $values);
+    }
+
+    /**
+     * @param  array $data
+     * @param  string $relation
+     */
+    public function updateSpecificHasManyRelation($data, $relation, $model = null)
+    {
+        return $this->repo->updateSpecificHasManyRelation($data, $relation, $model);
+    }
+
+    public function destroyFile($path, $id)
+    {
+        $this->deleteFile($path);
+        $this->repo->forceDelete($id);
+
+    }
+}
